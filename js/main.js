ae7c517e73a1fcf1405ddc988e954df65c2d61ef
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2021-10-09 10:51:55 +0200

    limpiando codigo V 1.1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
-function encender() {
-    document.getElementById("color").style.background = "greenyellow";
+function startStop() {
+    var comienza = document.getElementById("color").style.backgroundColor = ("greenyellow")
 }
 
+// function startStop() {
+//     var comienza = document.getElementById("color").style.backgroundColor = ("greenyellow")
+//     console.log(comienza);
+//     for (let i = 0; i < 2; i++) {
+//         if (comienza == "greenyellow") {
+//             comienza ==
+//         } else {
+//             var termina = document.getElementById("color").style.backgroundColor = ("#3c3c3c")
+//             console.log(termina);
+//         }
+
+//     }
+// }
+// function startStop(item) {
+//     if (item.className == "off__img") {
+//         item.className == "on__img"
+//     } else {
+//         item.className == "off__img"
+//     }
+
+// }
+// var apagar
+
+// function startStop() {
+//     let start = document.getElementById("color").style.background = "greenyellow";
+//     console.log(start);
+//     do {
+//         let stop = document.getElementById("color").style.background = "grey";
+//         console.log(stop);
+//     } while (start = "greenyellow");
+// }
 
 // function apagar() {
 //     document.getElementById("color").style.background = "grey";
-// }
-// apagar()
-
-// for (let i = 0; i < 2; i++) {
-//     if (encender ) {
-
-//     } else {
-
-//     }
-
 // }
